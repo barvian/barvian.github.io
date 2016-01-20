7db6406be856371b94c62a147fff575b7d99a133
Maxwell Barvian <max@barvian.me>
2016-01-20 14:24:24 -0600

    Work card styling

--- _assets/elements/work-card/work-card.js
+++ _assets/elements/work-card/work-card.js
@@ -3,28 +3,57 @@
 
 Polymer({
   is: 'work-card',
-  extends: 'article',
+
+  behaviors: [
+    Barvian.StylePropertiesBehavior
+  ],
 
   properties: {
     href: String,
     title: String,
-    bg: String,
-    fg: String
+    orientation: {
+      type: String,
+      computed: '_computeOrientation(aspect)'
+    }
   },
+
+  styleProperties: {
+    bg: String,
+    fg: String,
+    aspect: Number
+  },
+
+  observers: [
+    'updateLink(href)',
+    'updateOrientation(orientation)'
+  ],
 
   ready() {
-    if (this.href) {
-      this.innerHTML =
-        `<a href="${this.href}">
-          ${this.innerHTML}
-        </a>`;
-    }
-
-    this.customStyle['--work-card-bg'] = this.bg;
-    this.customStyle['--work-card-fg'] = this.fg;
+    this.updateLink(this.href);
   },
 
-  loaded(event) {
+  updateLink(href) {
+    if (!this.link) {
+      this.link = document.createElement('a');
+      Polymer.dom(this.$.wrapper).appendChild(this.link);
+      Polymer.dom(this.$.wrapper).childNodes
+        .filter(child => child !== this.link)
+        .forEach(child => Polymer.dom(this.link).appendChild(child));
+    }
+
+    Polymer.dom(this.link).setAttribute('href', href);
+  },
+
+  updateOrientation(orientation) {
+    this.classList.remove('portrait', 'landscape');
+    this.classList.add(orientation);
+  },
+
+  _imgLoaded(event) {
     this.classList.add('loaded');
+  },
+
+  _computeOrientation(aspect) {
+    return aspect > 1 ? 'portrait' : 'landscape';
   }
 });
--- _works/thesis.md
+++ _works/thesis.md
@@ -1,9 +1,11 @@
 ---
-title: Thesis
+title: Honors Thesis
 blurb: GAD self-help iPhone app
 type: iOS
 bg: '#f0f5f7'
 fg: '#454f5e'
+hero: hero.png
+aspect: 1.52
 ---
 
 Thesis stuff.
